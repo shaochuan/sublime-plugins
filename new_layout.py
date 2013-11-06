391ea018af8c202ff9f7dd785f24faff5a36eb31
Shao-Chuan Wang <shaochuan.wang@bloomreach.com>
2013-11-05 23:50:06 -0800

    It is window command, not view command.

--- new_layout.py
+++ new_layout.py
@@ -1,7 +1,8 @@
 import sublime, sublime_plugin
 
-class NewLayoutCommand(sublime_plugin.TextCommand):
-  def run(self, edit, **args):
-    self.view.window().run_command("set_layout", args)
-    self.view.window().run_command("focus_group", { "group": 0 })
-    self.view.window().run_command("move_to_group", { "group": 1 } )
+class NewLayoutCommand(sublime_plugin.WindowCommand):
+  def run(self, **args):
+    print(dir(self))
+    self.window.run_command("set_layout", args)
+    self.window.run_command("focus_group", { "group": 0 })
+    self.window.run_command("move_to_group", { "group": 1 } )
